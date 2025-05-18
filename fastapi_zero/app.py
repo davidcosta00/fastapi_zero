21800ed4b3f08793dd3142a268cd463f33fbc70c
davidcosta00 <45663282+davidcosta00@users.noreply.github.com>
2025-05-18 10:20:34 -0300

    adicionando schemas

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -1,8 +1,23 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+from fastapi_zero.schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá mundo!'}
+
+
+@app.get('/exercicio/2', response_class=HTMLResponse)
+def exercicio_segunda_aula():
+    return """
+    <html>
+        <body>
+            <div>Olá Mundo!</div>
+        </body>
+    </html>"""
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,6 @@
 from http import HTTPStatus
 
+from bs4 import BeautifulSoup as bs
 from fastapi.testclient import TestClient
 
 from fastapi_zero.app import app
@@ -11,3 +12,12 @@ def test_root_deve_retornar_ola_mundo():
     response = client.get('/')
     assert response.json() == {'message': 'Olá mundo!'}
     assert response.status_code == HTTPStatus.OK
+
+
+def test_exercicio_segunda_aula_ola_mundo():
+    client = TestClient(app)
+
+    response = client.get('/exercicio/2')
+    response = bs(response, 'html.parser')
+    assert response.status_code == HTTPStatus.OK
+    assert response.find({'div'}).text == 'Olá Mundo!'
